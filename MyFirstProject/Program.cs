a5944bb031e5d807d9c0b3bea157a779cfc3988e
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2025-04-15 11:48:40 +0200

    datatypesClass

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -17,6 +17,7 @@ namespace MyFirstProject
             // Auskommentieren = strg + #
 
             // Datatypes
+            // Integer
             DataTypes intType = new DataTypes();
             intType.TypeInteger();
             
