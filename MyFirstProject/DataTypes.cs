578f0ca946f0ff19895d3b80a3d4cc541d3e7e55
RaphaelStraschewski <r.Straschewski@edu.bbq.de>
2025-04-15 15:55:49 +0200

    datatypes formatiert

--- MyFirstProject/DataTypes.cs
+++ MyFirstProject/DataTypes.cs
@@ -15,24 +15,25 @@ namespace MyFirstProject
         // int age = Initialisation
         // 23 = Deklaration
         //Console.WriteLine(age);
-            Console.WriteLine();
-            Console.WriteLine("\"Datentypbereiche\"");
-            Console.WriteLine("Datentypbereich für int: " + int.MinValue + " bis " + int.MaxValue);
+        Console.WriteLine();
+        Console.WriteLine("\"Datentypbereiche\"");
+        Console.WriteLine("Datentypbereich für int: " + int.MinValue + " bis " + int.MaxValue);
 
-            long bigNumber = 900000000L;
+        long bigNumber = 900000000L;
         // Das L signalisiert eine Int64(Bit), sonst Int32(Bit) => nicht gleicher Datentypbereich
         //Console.WriteLine(bigNumber);
         Console.WriteLine("Datentypbereich für long: " + long.MinValue + " bis " + long.MaxValue);
 
-            double neagtive = 55.2D;
+        double neagtive = 55.2D;
         // D steht für Double
         //Console.WriteLine(neagtive);
-            Console.WriteLine("Datentypbereich für double: " + double.MinValue + " bis " + double.MaxValue);
+        Console.WriteLine("Datentypbereich für double: " + double.MinValue + " bis " + double.MaxValue);
 
-            float precision = 5.000001F;
+        float precision = 5.000001F;
         // F steht für Float; default ist D (double)
         //Console.WriteLine(precision);
-            Console.WriteLine("Datentypbereich für float: " + float.MinValue + " bis " + float.MaxValue);
+        Console.WriteLine("Datentypbereich für float: " + float.MinValue + " bis " + float.MaxValue);
+
         }
     }
 }
